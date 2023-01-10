d438a829882647573255fec4ef652a654eb75739
Minji Kim <110662771+sloane323@users.noreply.github.com>
2023-01-10 15:31:09 +0900

    add menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import SideBar from './components/SideBar/SideBar';
-import Users from './components/Users';
+import { Routes, Route } from "react-router-dom";
+import Main from './Page/Main';
+import Dashboard from './Page/Dashboard';
+import Production from './Page/Production';
+import Sales from './Page/Sales';
+import Customers from './Page/Customers';
+import Reports from './Page/Reports';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
-      <Users/>
+      
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/production" element={<Production />}></Route>
+        <Route path="/sales" element={<Sales />}></Route>
+        <Route path="/customers" element={<Customers />}></Route>
+        <Route path="/reports" element={<Reports />}></Route>
+      </Routes>
+
     </div>
   );
 }
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,8 @@
+import styles from "../SideBar/SideBar.module.css";
 const SideBar = () => {
     return ( 
-        <div>
-            <div> 대시보드 </div>
+        <div className={styles.sidebar}>
+            <div className={styles.dashboard}> 대시보드 </div>
             <div> 생산 </div>
             <div> 판매 </div>
             <div> 고객 </div>
