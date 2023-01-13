2b3c81f8feda7cd1502c2c52b978e7ae4ebbc5fb
Johnny_P <shinezl39@gmail.com>
2023-01-13 17:41:51 +0900

    원자재, 재고 테이블 각각 접근해서 받아오는 중

--- src/Page/Production/Stock.jsx
+++ src/Page/Production/Stock.jsx
@@ -5,8 +5,9 @@ import { useEffect } from "react";
 
 const Stock = () => {
   const [stocks, setStocks] = useState(null);
+  const [products, setProducts] = useState(null);
 
-  const getStock = async () => {
+  const getStocks = async () => {
     try {
       const response = await axios.get(`/api/material`);
       /* console.log(response.data) */
@@ -16,13 +17,23 @@ const Stock = () => {
     }
   };
 
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(`/api/production`);
+      setProducts(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
-    getStock();
+    getStocks();
+    getProducts();
   }, []);
 
   return (
     <div>
-      <h1>생산 - 상품 재고 확인</h1>
+      <h1>원자재 재고 확인</h1>
       <table>
         <thead>
           <tr>
@@ -40,6 +51,27 @@ const Stock = () => {
             <td>{stock.가격}</td>
             <td>{stock.브랜드}</td>
             <td><Link to={`/production/stock/${stock.원자재ID}`}>▶재고확인(detail)</Link></td>
+          </tr>) :
+          <tr>
+            <td>NOT YET</td>
+          </tr>}
+        </tbody>
+      </table>
+      <h1>제품 재고 확인</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>이름</td>
+            <td>사이즈</td>
+            <td>가격</td>
+            <td>브랜드</td>
+          </tr>
+        </thead>
+        <tbody>
+          {products ? products.map((product) =>
+          <tr key={product.제품ID}>
+            <td>{product.제품ID}</td>
+            <td><Link to={`/production/stock/${product.제품ID}`}>▶재고확인(detail)</Link></td>
           </tr>) :
           <tr>
             <td>NOT YET</td>
