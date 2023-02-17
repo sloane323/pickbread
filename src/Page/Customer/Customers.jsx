af9c34cb526218e23cf4318c36bdeba591f90dec
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-02-17 11:27:45 +0900

    server의 post 메서드 복원

--- src/Page/Customer/Customers.jsx
+++ src/Page/Customer/Customers.jsx
@@ -2,22 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 const Customers = () => {
-
   // 고객 조회 화면 출력을 위한 state
-const [customers, setCustomers] = useState("");
+  const [customers, setCustomers] = useState("");
   // 고객 조회 함수
-const getCustomer =()=> {
-  axios.get('/api/customer')
-  .then(res => setCustomers(res.data))
-}
+  const getCustomer = () => {
+    axios.get("/api/customer").then((res) => setCustomers(res.data));
+  };
   // 렌더되면 바로 조회
-useEffect(()=>{
-  getCustomer();
-},[]);
-
+  useEffect(() => {
+    getCustomer();
+  }, []);
 
   return (
     <div>
@@ -33,38 +28,34 @@ useEffect(()=>{
         <input type="text" />
         <button> 검색 </button>
       </div>
-      <div>
-
-      </div>
+      <div></div>
       <h2>조회</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>no</th>
-                            <th>고객명</th>
-                            <th>전화번호</th>
-                            <th>포인트</th>
-                            <th>기타</th>
-                            <th>설정</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        customers && customers.map((d, idx)=>(
-                        <tr key={idx}>
-                            <td>{idx+1}</td>
-                            <td>{d.이름}</td>
-                            <td>{d.전화번호}</td>
-                            <td>0</td>
-                            <td>{d.코멘트}</td>
-                            <td>.</td>
-                        </tr>
-                        ))
-                    }
-                    </tbody>
-                </table>
-            </div>
-
+      <table>
+        <thead>
+          <tr>
+            <th>no</th>
+            <th>고객명</th>
+            <th>전화번호</th>
+            <th>포인트</th>
+            <th>기타</th>
+            <th>설정</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers &&
+            customers.map((d, idx) => (
+              <tr key={idx}>
+                <td>{idx + 1}</td>
+                <td>{d.이름}</td>
+                <td>{d.전화번호}</td>
+                <td>0</td>
+                <td>{d.코멘트}</td>
+                <td>.</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
