bf61658d0cbbdeccf5e988a8a4e7e70835a42b63
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-02-20 19:55:30 +0900

    유효기간 초기 값 설정

--- src/common.js
+++ src/common.js
@@ -5,9 +5,17 @@ export const getNowDate = () => {
   return new Date().toISOString().slice(0, 10);
 };
 export const getTwoWeeksDate = (dateValue) => {
-  const dateInstance = new Date(dateValue);
-  const year = dateInstance.getFullYear(); // 년
-  const month = dateInstance.getMonth();   // 월
-  const day = dateInstance.getDate();      // 일
-  return new Date(year, month, day + 15).toISOString().slice(0, 10);
-}
+  if (dateValue) {
+    const dateInstance = new Date(dateValue);
+    const year = dateInstance.getFullYear(); // 년
+    const month = dateInstance.getMonth(); // 월
+    const day = dateInstance.getDate(); // 일
+    return new Date(year, month, day + 15).toISOString().slice(0, 10);
+  } else {
+    const dateInstance = new Date();
+    const year = dateInstance.getFullYear(); // 년
+    const month = dateInstance.getMonth(); // 월
+    const day = dateInstance.getDate(); // 일
+    return new Date(year, month, day + 15).toISOString().slice(0, 10);
+  }
+};
