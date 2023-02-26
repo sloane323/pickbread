3c666d043956e65bf52fb5192fd0c47d114fecb2
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-02-26 18:17:04 +0900

    폴더 재지정 중

--- src/Page/Production/IndexRoutes/TotalStocksIndexPage.jsx
+++ src/Page/Production/IndexRoutes/TotalStocksIndexPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductPage = () => {
+const TotalStocksIndexPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ProductPage = () => {
   )
 }
 
-export default ProductPage
+export default TotalStocksIndexPage
