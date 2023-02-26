b249de87a3da490de52e39fb36140e208178445f
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-02-26 20:03:10 +0900

    세부 페이지 재업로드

---
+++ src/Page/Production/MaterialPages/MaterialDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GetMaterialDetail from '../../../components/GetStocks/GetDetails/GetMaterialDetail'
+
+const MaterialDetails = () => {
+  return (
+    <div>
+      <GetMaterialDetail></GetMaterialDetail>
+    </div>
+  )
+}
+
+export default MaterialDetails
