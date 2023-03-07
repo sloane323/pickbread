5f9c427064699ef0a76a318d38eedc8e1a76ab07
PigPenguinMan <bigpenguinman@naver.com>
2023-03-07 10:58:34 +0900

    03/02

--- src/Page/Dashboard.jsx
+++ src/Page/Dashboard.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 const Dashboard = () => {
   const [vendor, setVendor] = useState();
-  const [purchasing, setPurchasing] = useState();
+  const [purchasing, setPurchasing] = useState([]);
   const [customer, setCustomer] = useState();
   const [p_stock, setP_stock] = useState();
   const [product, setProduct] = useState();
-  const [today, setToday] = useState(new Date().toISOString().slice(0, 10));
+  const [dateArray, setDateArray] = useState([]);
+  const [chunkedPurchasing, setChunkedPurchasing] = useState([]);
 
+  const [currentStock, setCunrrentStock] = useState();
+  const [chunkedStock, setChunkedStock] = useState([]);
+  const [dispStock, setDisplayStock] = useState([]);
   // 거래처
   const getVendor = async () => {
     const url = "/api/vendor";
@@ -43,61 +48,86 @@ const Dashboard = () => {
     setProduct(response.data);
   };
 
+  // 원자재생산 받아오기
+  const getCurrnetStock = async () => {
+    const url = "/api/CurrentStock";
+    const response = await axios.get(url);
+    setCunrrentStock(response.data);
+  };
+
   useEffect(() => {
     getVendor();
     getPurchasing();
     getCustomer();
     getP_stock();
     getProduct();
+    getCurrnetStock();
   }, []);
 
-  // 날짜 비교하는 함수
-  const dateDiff = (d1, d2) => {
-    const date1 = new Date(d1);
-    const date2 = new Date(d2);
-    const diffDate = date1.getTime() - date2.getTime();
-    return Math.abs(diffDate / (1000 * 60 * 60 * 24));
-  };
+  useEffect(() => {
+    // 구매일 기준으로 내림차순 정렬
+    const sortedPurchasing = [...purchasing].sort((a, b) => {
+      const dateA = new Date(a.구매일);
+      const dateB = new Date(b.구매일);
+      return dateB - dateA;
+    });
 
-  // 원자재 구매 날짜 비교
-  const purchasingDate = () => {
-    // for (let i = 0; i < purchasing.length; i++) {
-    //   const PDate = purchasing[i].구매일;
-    //   console.log(dateDiff(today, PDate));
-    // }
-    // 정렬하는함수
-    purchasing.sort(function (a, b) {
-      if (a > b) return 1;
-      if (a === b) return 0;
-      if (a < b) return -1;
+    // 5개씩 잘라서 저장
+    const chunkSize = 5;
+    const chunkedArray = [];
+    for (let i = 0; i < sortedPurchasing.length; i += chunkSize) {
+      chunkedArray.push(sortedPurchasing.slice(i, i + chunkSize));
+    }
+    setChunkedPurchasing(chunkedArray);
+  }, [purchasing]);
+
+  // 사용기한 기준으로 내림차순
+  const displayStock = () => {
+    const sortedStock = currentStock.map((stock) => {
+      return [...stock].sort((a, b) => {
+        const dateA = new Date(a.사용기한);
+        const dateB = new Date(b.사용기한);
+        return dateA - dateB;
+      });
     });
   };
-
   const colog = () => {
     console.log("vendor", vendor);
     console.log("purchasing", purchasing);
     console.log("customer", customer);
     console.log("p_stock", p_stock);
     console.log("product", product);
-    console.log("today", today);
+    console.log("currentStock", currentStock);
   };
+
   return (
     <div>
       Dashboard
       <button
         onClick={() => {
           colog();
+          displayStock();
         }}
       >
         테스트용
       </button>
-      <button
-        onClick={() => {
-          purchasingDate();
-        }}
-      >
-        구매날짜
-      </button>
+      {chunkedPurchasing.map((chunk, index) => (
+        <div key={index}>
+          <h2>구매내역 {index + 1}</h2>
+          {chunk.map((item, innerIndex) => (
+            <p key={innerIndex}>{item.구매일}</p>
+          ))}
+        </div>
+      ))}
+      <h2>원자재갯수</h2>
+      {currentStock !== undefined &&
+        currentStock.map((a) => (
+          <div>
+            <p>
+              {a.종류} {a.사용기한}
+            </p>
+          </div>
+        ))}
     </div>
   );
 };
