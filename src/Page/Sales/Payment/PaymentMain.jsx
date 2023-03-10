f370ad26e41c8f171f8a8ff2fa5fee14d07db8e8
PigPenguinMan <bigpenguinman@naver.com>
2023-03-10 16:50:41 +0900

    03/10 consolelog삭제

--- src/Page/Sales/Payment/PaymentMain.jsx
+++ src/Page/Sales/Payment/PaymentMain.jsx
@@ -14,7 +14,6 @@ const PaymentMain = (props) => {
   useEffect(() => {
     getP_stock()
   }, []);
-  console.log('selectedCustomer',selectedCustomer);
   const handleClosePage=()=>{
     props.modalHandler2()
   }
