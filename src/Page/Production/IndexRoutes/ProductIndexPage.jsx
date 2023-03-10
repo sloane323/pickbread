abfb04ae4b9572d4f2589c13245193f0d0f12d44
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-03-10 19:43:51 +0900

    생산 페이지 탭 구분

--- src/Page/Production/IndexRoutes/ProductIndexPage.jsx
+++ src/Page/Production/IndexRoutes/ProductIndexPage.jsx
@@ -1,16 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import AddProductsType from "../ProductPages/AddProductsType";
 import CurrentProductTypes from "../ProductPages/CurrentProductTypes";
 import ManufactureProducts from "../ProductPages/ManufactureProducts";
 import CurrentProductStocks from "../ProductPages/CurrentProductStocks";
 
 const ProductIndexPage = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const compArray = [
+    {
+      tabBttn: (
+        <button
+          className={activeIndex === 0 ? "active22" : ""}
+          onClick={() => setActiveIndex(0)}
+        >
+          제품 종류 등록
+        </button>
+      ),
+      tabComp: (
+        <div>
+          <br></br>
+          <AddProductsType></AddProductsType>
+        </div>
+      ),
+    },
+    {
+      tabBttn: (
+        <button
+          className={activeIndex === 1 ? "active22" : ""}
+          onClick={() => setActiveIndex(1)}
+        >
+          현재 제품 종류 조회
+        </button>
+      ),
+      tabComp: (
+        <div>
+          <br></br>
+          <CurrentProductTypes></CurrentProductTypes>
+        </div>
+      ),
+    },
+    {
+      tabBttn: (
+        <button
+          className={activeIndex === 2 ? "active22" : ""}
+          onClick={() => setActiveIndex(2)}
+        >
+          제품 생산
+        </button>
+      ),
+      tabComp: (
+        <div>
+          <br></br>
+          <ManufactureProducts></ManufactureProducts>
+        </div>
+      ),
+    },
+    {
+      tabBttn: (
+        <button
+          className={activeIndex === 3 ? "active22" : ""}
+          onClick={() => setActiveIndex(3)}
+        >
+          현재 제품 재고
+        </button>
+      ),
+      tabComp: (
+        <div>
+          <br></br>
+          <CurrentProductStocks></CurrentProductStocks>
+        </div>
+      ),
+    },
+  ];
   return (
     <div>
-      <AddProductsType></AddProductsType>
-      <CurrentProductTypes></CurrentProductTypes>
-      <ManufactureProducts></ManufactureProducts>
-      <CurrentProductStocks></CurrentProductStocks>
+      {compArray.map((comp) => (comp.tabBttn))}
+      <hr />
+      {compArray[activeIndex].tabComp}
     </div>
   );
 };
