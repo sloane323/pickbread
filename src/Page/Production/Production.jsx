01df8b07df4f7ccf8f25c97b673861fbba786966
Minji Kim <110662771+sloane323@users.noreply.github.com>
2023-04-22 08:05:13 +0800

    update css

--- src/Page/Production/Production.jsx
+++ src/Page/Production/Production.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import styles from "./Production.module.css";
+
 // 생산 페이지
 const Production = () => {
   const navigate = useNavigate();
@@ -14,8 +16,10 @@ const Production = () => {
   }
 
   return (
-    <div>
+    <div className={styles.producrtionmain}>
+      <div className={styles.ineer}>
       <h1>원자재, 제품, 재고</h1>
+      <div className={styles.buttonlist}>
       <div>
         <button onClick={goToMaterialIndexPage}>원자재 페이지</button>
       </div>
@@ -25,7 +29,9 @@ const Production = () => {
       <div>
         <button onClick={goToTotalStocksPage}>원자재 및 제품 통합 재고</button>
       </div>
-    </div>
+      </div>
+        </div>
+      </div>
   );
 };
 
